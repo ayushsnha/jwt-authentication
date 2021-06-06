555fe821c366e8714bf2454ce359f2b0ccf4384f
ayushsnha <ayush_snha@rediffmail.com>
2021-06-06 21:03:57 +0530

    refresh token and logout

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ require('dotenv').config()
 const express= require('express');
 const app= express();
 
-const jwt = require('jsonwebtoken');
-
 const authenticateToken = require('./authenticateToken');
 
 app.use(express.json());
@@ -22,15 +20,6 @@ const posts= [
 
 app.get('/posts', authenticateToken, (req,res)=> {
     res.json(posts.filter(post=> post.userName===req.user.name))
-})
-
-app.post('/login', (req,res)=>{
-    //Authenticate user
-
-    const userName= req.body.userName;
-    const user = {name: userName}
-    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
-    res.json({accessToken:accessToken})
 })
 
 app.listen(3000,()=>console.log('server started'))
