08965708efe2bee2e2046ae2adc3a94b87b751be
ayushsnha <ayush_snha@rediffmail.com>
2021-06-07 21:02:43 +0530

    connecting to mongodb and storing user data and refresh token to db

--- auth.js
+++ auth.js
@@ -1,13 +1,27 @@
 require('dotenv').config()
-
 const express= require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+
 const app= express();
 
 const jwt = require('jsonwebtoken');
 
 app.use(express.json());
 
+const User = require('./models/user')
+
 let refreshTokens=[];
+
+mongoose.connect(process.env.MONGOURI,{
+    useNewUrlParser:true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+    })
+    .then(()=> console.log('Connected to Database!!'))
+    .catch(err=>console.log(err))    
 
 app.post('/token', (req,res)=> {
     const refreshToken = req.body.token;
@@ -25,15 +39,71 @@ app.delete('/logout', (req,res)=> {
     res.sendStatus(204)
 })
 
-app.post('/login', (req,res)=>{
-    //Authenticate user
+app.post('/register', (req,res)=>{
+    const { name, email, password}= req.body;
+    if(!name || !email || !password)
+        return res.status(400).json({message: "Invalid field"})
 
-    const userName= req.body.userName;
-    const user = {name: userName}
-    const accessToken = generateAccessToken(user);
-    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
-    refreshTokens.push(refreshToken);
-    res.json({accessToken:accessToken, refreshToken:refreshToken})
+    User.findOne({email})
+    .then(user=>{
+        if(user) return res.status(400).json({message: 'User already exists'});
+        const newUser = new User({
+            name,
+            email,
+            password
+        });
+
+        bcrypt.hash(newUser.password, 10, (err,hash)=>{
+            console.log(hash)
+            newUser.password = hash;
+            newUser.save()
+            .then(user=>{
+                const accessToken = generateAccessToken({id: user._id});
+                const refreshToken = jwt.sign({id: user._id}, process.env.REFRESH_TOKEN_SECRET)
+                User.findOneAndUpdate({_id: user._id},{refresh_token: refreshToken})
+                .then(user=>console.log(user));
+                res.json({
+                    accessToken,
+                    refreshToken,
+                    user: {
+                        id: user._id,
+                        name: user.name,
+                        email: user.email
+                    }
+                })
+            })
+            .catch(err=>console.log(err)) 
+        })
+    })
+
+})
+
+app.post('/login', (req,res)=>{
+    const {email, password}= req.body;
+    if(!email || !password)
+        return res.status(400).json({message: "Invalid field"})
+
+    User.findOne({email})
+    .then(user=>{
+        if(!user) return res.status(400).json({message: 'User not exists'});
+        bcrypt.compare(password, user.password)
+        .then(isMatch=>{
+            if(!isMatch) return res.status(400).json({message: 'Invalid Credential!!'});
+            const accessToken = generateAccessToken({id: user._id});
+            const refreshToken = jwt.sign({id: user._id}, process.env.REFRESH_TOKEN_SECRET,{expiresIn: '500s'});
+            User.findOneAndUpdate({_id: user._id},{refresh_token: refreshToken}, {new:true})
+            .then(user=>console.log(user));
+            res.json({
+                accessToken,
+                refreshToken,
+                user: {
+                    id: user._id,
+                    name: user.name,
+                    email: user.email
+                }
+            })
+        })
+    })
 })
 
 function generateAccessToken(user) {
